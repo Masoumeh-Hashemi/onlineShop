e167ec571a87da5887eadc42bdbf1c951146473a
hashemi <masoumeh.hashemi92@gmail.com>
2020-11-07 18:39:22 +0330

    add city :)

--- packages/server/src/Controller/City/addCity.ts
+++ packages/server/src/Controller/City/addCity.ts
@@ -1,11 +1,30 @@
-// import { cities } from './../../Schema/City.ts';
-// import { Context } from 'https://deno.land/x/oak@v6.3.1/mod.ts';
-// import { create } from "../base/create.ts";
 
-export const addCity = ()=>{
-    console.log("=============================");
-    console.log("da");
-    console.log("=============================");
-    
-    //  return create(Context,cities)
+import { create } from "../base/create.ts";
+import { Context } from "https://deno.land/x/oak@v6.3.1/mod.ts";
+import { cities } from "../../Schema/City.ts";
+import { checkReqBody } from "../functions/checkReqBody.ts";
+import { getBodyValueOfReq } from "../functions/getBodyValueOfReq.ts";
+import { SetSuccessReqBody } from "../functions/SetSuccessReqBody.ts";
+import { setFailedReqBody } from "../functions/setFailedReqBody.ts";
+
+
+export const addCity =async (ctx:Context)=> {
+
+try{    
+     if(await checkReqBody(ctx)===true){
+     const json =await getBodyValueOfReq(ctx)
+     if(typeof json === 'object'){
+       const creation = await create(cities,json) 
+        SetSuccessReqBody(creation,ctx)
+     }
+     else{
+      setFailedReqBody(ctx)
+     }
+   
+     }
+  }
+catch  {
+ await setFailedReqBody(ctx)
+
+  }
 }
